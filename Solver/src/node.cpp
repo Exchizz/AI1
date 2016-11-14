1f805c3cee12b992e87f950dc9bcc17fe1be2c96
Mathias Neerup <manee12@student.sdu.dk>
2016-11-14 01:35:33 +0100

    2015 solved in 12 min, 2014 solved in 19 min

--- Solver/src/node.cpp
+++ Solver/src/node.cpp
@@ -4,6 +4,15 @@ Node::Node(Point PosMan, std::vector<Point> &PosJew){
   this->PosMan = PosMan;
   this->PosJew = PosJew;
   //children.reserve(8); // slows down
+/*
+  if(PosMan.x == 1 or PosMan.x == 6){
+    std::cout << "Box in outersides" << PosMan<< std::endl;
+  }
+
+  if(PosMan.y == 1 or PosMan.y == 6){
+    std::cout << "Box in upper/lower sides:" << PosMan << std::endl;
+  }
+  */
 }
 [[deprecated]]
 void Node::Insert(Map & map,Point PosMan, std::vector<Point> &PosJew){
